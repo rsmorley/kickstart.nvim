179b10e1f0a87890ba85f0ab49e4ed91c4db1fd6
Scoot <morley.rs@gmail.com>
2024-11-10 21:11:56 -0700

    Add file missed in last commit

---
+++ lua/custom/plugins/snacks.lua
@@ -0,0 +1,29 @@
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  opts = {
+    bigfile = { enabled = true },
+    notifier = {
+      enabled = true,
+      timeout = 3000,
+    },
+    quickfile = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+    styles = {
+      notification = {
+        wo = { wrap = true }, -- Wrap notifications
+      },
+    },
+  },
+  keys = {
+    {
+      '<c-x>',
+      function()
+        Snacks.terminal()
+      end,
+      desc = 'Toggle Terminal',
+    },
+  },
+}
