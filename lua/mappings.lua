8e866de022c88e93c7beaa180bf773e5981eea25
Scoot <morley.rs@gmail.com>
2024-11-10 20:01:09 -0700

    added mappings and options file

---
+++ lua/mappings.lua
@@ -0,0 +1,10 @@
+vim.keymap.set('n', '<C-s>', '<CMD>w<CR>')
+vim.keymap.set('n', '<leader>q', '<CMD>q<CR>')
+
+vim.keymap.set('n', '<C-x>', ':split<BAR>:terminal<CR>')
+
+-- window navigation
+vim.keymap.set('n', '<C-left>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-right>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-down>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-up>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
