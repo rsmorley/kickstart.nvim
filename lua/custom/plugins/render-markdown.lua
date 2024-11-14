069d0871287b0f5edeebcb147275bdabe2f5b287
Scoot <morley.rs@gmail.com>
2024-11-14 09:26:12 -0700

    Add custom inprogress checkbox to render-markdown

--- lua/custom/plugins/render-markdown.lua
+++ lua/custom/plugins/render-markdown.lua
@@ -4,7 +4,27 @@ return {
     lazy = true,
     enabled = true,
     ft = 'markdown',
-    opts = {},
+    opts = {
+      checkbox = {
+        enabled = true,
+        position = 'inline',
+        unchecked = {
+          icon = '󰄱 ',
+          highlight = 'RenderMarkdownUnchecked',
+          scope_highlight = nil,
+        },
+        checked = {
+          icon = '󰱒 ',
+          highlight = 'RenderMarkdownChecked',
+          scope_highlight = nil,
+        },
+        custom = {
+          todo = { raw = '[-]', rendered = '󰥔 ', highlight = 'RenderMarkdownTodo', scope_highlight = nil },
+          -- inprogress = { raw = '[/]', rendered = '', highlight = 'RenderMarkdownInProgress', scope_highlight = nil },
+          inprogress = { raw = '[/]', rendered = '󰿦 ', highlight = 'DiagnosticWarn', scope_highlight = nil },
+        },
+      },
+    },
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
