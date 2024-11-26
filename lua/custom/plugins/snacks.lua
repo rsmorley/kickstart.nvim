6c54e469819eee9852c3629b13c2f011990b11f9
Scoot <morley.rs@gmail.com>
2024-11-25 21:00:36 -0700

    Add snacks dashboard, disable snacks notify because it doesn't handle confirmations

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -4,8 +4,17 @@ return {
   lazy = false,
   opts = {
     bigfile = { enabled = true },
-    notifier = {
+    dashboard = {
       enabled = true,
+      sections = {
+        { section = 'header' },
+        { section = 'keys', gap = 1 },
+        { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = { 2, 2 } },
+        { section = 'startup' },
+      },
+    },
+    notifier = {
+      enabled = false, -- doesn't handle confirmations
       timeout = 5000,
       width = { min = 40, max = 0.6 },
     },
